738a221d97767ab5b077b21ec01f976cc6b2518c
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-08-09 21:32:47 +0300

    build: add Vike plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vike from 'vike/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vike()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+      '@pages': '/pages',
+      '@renderer': '/renderer',
+    },
+  },
 })
