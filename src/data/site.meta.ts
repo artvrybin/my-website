5e7dbc858effbe8219d8fe2301f9fefb5131c395
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-08-10 18:33:18 +0300

    refactor: upd meta about me

--- src/data/site.meta.ts
+++ src/data/site.meta.ts
@@ -1,20 +1,16 @@
-import type { SiteMeta } from '@/types/site'
+import type {SiteMeta} from '@/types/site'
 
 export const siteMeta: SiteMeta = {
-  name: 'Рыбин Артем',
-  role: 'Frontend‑разработчик',
-  city: 'Иваново',
-  bio:
-    'Frontend‑разработчик с 2‑летним опытом. TypeScript, React, Next.js, Redux Toolkit, RTK Query. Стремлюсь создавать чистый, поддерживаемый и доступный UI.',
-  introLines: [
-    'Hello! My name is Artem Rybin.',
-    'I am a frontend developer.',
-    'I can do some great',
-    'things for you.'
-  ],
-  ctaText: "Let’s check!",
-  ctaHref: '#portfolio',
-  techStack: ['TS', 'React', 'SCSS', 'JS'],
+    name: 'Рыбин Артем',
+    role: 'Frontend‑разработчик',
+    city: 'Иваново',
+    bio:
+        'Frontend‑разработчик с 2‑летним опытом. TypeScript, React, Next.js, Redux Toolkit, RTK Query. Стремлюсь создавать чистый, поддерживаемый и доступный UI.',
+    introLines: [
+        'Hello! My name is',
+        'Artem Rybin.',
+        'I am a frontend developer.'
+    ],
 }
 
 
